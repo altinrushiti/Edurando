ded7eab3a024cf47676ca32af837ddcb81b42fb1
Krish <krish.kalra@stud.th-luebeck.de>
2023-04-26 11:53:36 +0200

    Backend

--- Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/login/LoginController.java
+++ Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/login/LoginController.java
@@ -2,8 +2,10 @@ package de.app_solutions.Edurando.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LoginController {
 
     @GetMapping("/login")
