22d5758140c485b9408d3ac47b124d47d3f9e04e
Zainoul Abidine Barry <abidine9111@gmail.com>
2023-04-29 20:03:24 +0200

    NavBar + RegistrationPage

---
+++ Edurando_App/edurando-frontend/src/modules/Footer/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="bg-[#483d8b] text-white">
+    <div class="flex justify-around items-center p-2">
+      <div class="logo flex items-center">
+        <img class="w-[100px] h-[60px]" src="@/assets/logo/logo_image.png" alt="Logo" />
+        <h1 class="text-3xl font-medium">Edurando</h1>
+      </div>
+      <ul class="flex justify-around gap-2 m-2 text-2xl">
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
