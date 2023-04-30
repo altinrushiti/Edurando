33b093d4f93ab1113169d55c03d82a6d19bcfad6
349923 <sena.demircik@stud.th-luebeck.de>
2023-04-30 18:39:04 +0200

    *Footer implemented; fontawesome libraries added

--- Edurando_App/edurando-frontend/src/App.vue
+++ Edurando_App/edurando-frontend/src/App.vue
@@ -2,11 +2,15 @@
 
 import NavBar from "./modules/NavBar/components/NavBar.vue";
 import RegistrationPage from "@/modules/Registration/page/RegistrationPage.vue";
+import Footer from "@/modules/Footer/Footer.vue"
+import '@fortawesome/fontawesome-free/css/all.css'
+
 </script>
 
 <template>
 <NavBar />
   <RegistrationPage />
+<Footer />
 </template>
 
 <style scoped>
