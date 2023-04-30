db6f5d4fe1f126930e11ecef84a6314655ff78f8
Sena <sena.demircik@stud.th-luebeck.de>
2023-04-30 21:38:00 +0200

    +* test

--- Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/controller/RegistrationController.java
+++ Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/controller/RegistrationController.java
@@ -21,6 +21,7 @@ public class RegistrationController {
         return registrationService.register(request);
     }
 
+
     @GetMapping(path = "confirm")
     public String confirm(@RequestParam("token") String token) {
         return registrationService.confirmToken(token);
