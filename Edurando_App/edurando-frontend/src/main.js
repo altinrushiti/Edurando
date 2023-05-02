5edbbf360557246ec5a637966cd71cbdc7f2df72
Sena <sena.demircik@stud.th-luebeck.de>
2023-05-02 11:32:46 +0200

    *NavBar Menu, Font, Icons changed

--- Edurando_App/edurando-frontend/src/main.js
+++ Edurando_App/edurando-frontend/src/main.js
@@ -1,19 +1,15 @@
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faFacebook, faTwitter, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp, faHome, faCircleInfo, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import '@fortawesome/fontawesome-free/css/all.css';
 import router from './router'
-
 import App from './App.vue';
-
 import './assets/main.css';
 
-// Add the necessary icons to the fontawesome library
-library.add(faFacebook, faTwitter, faInstagram, faTiktok, faArrowUp);
+library.add(faFacebook, faTwitter, faInstagram, faTiktok, faArrowUp, faHome, faCircleInfo, faBars, faTimes);
 
-// Create the app and register the FontAwesomeIcon component globally
 const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
