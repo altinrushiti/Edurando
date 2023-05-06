66f5e1d40e4f7c0e18b6b2a51f25ae659fd538a8
Krish <krish.kalra@stud.th-luebeck.de>
2023-05-06 17:29:19 +0200

    E1.6: JUnit Test

--- Edurando_App/Edurando/src/test/java/de/app_solutions/Edurando/service/EmailValidatorTest.java
+++ Edurando_App/Edurando/src/test/java/de/app_solutions/Edurando/service/EmailValidatorTest.java
@@ -1,5 +1,50 @@
 package de.app_solutions.Edurando.service;
 
+import de.app_solutions.Edurando.model.UserProfile;
+import de.app_solutions.Edurando.repository.UserProfileRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 public class EmailValidatorTest {
+    @Test
+    public void testUniqueMail() {
+        // Create a mock user profile repository
+        List<UserProfile> users = new ArrayList<>();
+        users.add(new UserProfile("Student", "Max", "Mustermann","max.mustermann@example.com","password"));
+        users.add(new UserProfile("Student", "Max", "Musterfrau","max.musterfrau@example.com","password"));
+        UserProfileRepository userProfileRepository = mock(UserProfileRepository.class);
+        when(userProfileRepository.findAll()).thenReturn(users);
+        EmailValidator emailValidator = new EmailValidator(userProfileRepository);
+        // Test unique email
+        boolean result1 = emailValidator.uniqueMail("max.musterfrau@example.com");
+        assertFalse(result1);
+
+        // Test non-unique email
+        boolean result2 = emailValidator.uniqueMail("john.doe@example.com");
+        assertTrue(result2);
+    }
+
+    @Test
+    public void testMail() {
+        UserProfileRepository userProfileRepository = mock(UserProfileRepository.class);
+        when(userProfileRepository.findAll()).thenReturn(Collections.emptyList());
+        EmailValidator emailValidator = new EmailValidator(userProfileRepository);
+        // Test unique email
+        boolean result1 = emailValidator.testMail("max.musterfrau@stud.th-luebeck.de");
+        assertTrue(result1);
+
+        // Test non-unique email
+        boolean result2 = emailValidator.testMail("john.doe@example.com");
+        assertFalse(result2);
+    }
 }
