b8a07dcfc917b80fdc7453a12699132c1868fa32
Zainoul Abidine Barry <abidine9111@gmail.com>
2023-05-10 10:20:55 +0200

    GetUser Profile

--- Edurando_App/edurando-frontend/src/modules/UserUpdate/EditPage.vue
+++ Edurando_App/edurando-frontend/src/modules/UserUpdate/EditPage.vue
@@ -5,7 +5,7 @@
             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
         </svg>
     </button>
-    <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-50 h-screen transition-transform -translate-x-full sm:translate-x-0 mt-16" aria-label="Sidebar">
+    <aside id="default-sidebar" class="fixed  top-0 left-0 z-40 w-50 h-[50%] transition-transform -translate-x-full sm:translate-x-0 mt-16" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-purple-50">
             <ul class="space-y-2 font-medium text-[#483d8b]">
                 <li>
@@ -29,6 +29,7 @@
             </ul>
         </div>
     </aside>
+
 </template>
 
 <script >
