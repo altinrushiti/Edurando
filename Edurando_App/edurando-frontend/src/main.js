4077b11d2789584887404b5651351839844ba131
Krish <krish.kalra@stud.th-luebeck.de>
2023-05-22 23:34:02 +0200

    E2: Quarsar einrichten

--- Edurando_App/edurando-frontend/src/main.js
+++ Edurando_App/edurando-frontend/src/main.js
@@ -11,6 +11,25 @@ import './assets/main.css';
 import "./axios"
 import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+
+// A few examples for animations from Animate.css:
+// import @quasar/extras/animate/fadeIn.css
+// import @quasar/extras/animate/fadeOut.css
+
+/*// Import Quasar css
+import 'quasar/src/css/index.sass'*/
+
 library.add(faFacebook, faTwitter, faInstagram, faTiktok, faArrowUp, faHome, faCircleInfo, faBars, faTimes, faArrowLeft, faUser, faKey, faTableList, faAnglesRight, faAnglesLeft,faCirclePlus,faCheck)
 
 const pinia = createPinia()
@@ -19,4 +38,7 @@ const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 app.use(pinia)
+app.use(Quasar, {
+    plugins: {}
+})
 app.mount('#app');
--- Edurando_App/edurando-frontend/src/view/Home.vue
+++ Edurando_App/edurando-frontend/src/view/Home.vue
@@ -8,8 +8,17 @@ const userStore = useUserStore();
 
 <template>
     <div class="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-4xl font-font-family text-slate-900">Welcome to Edurando</h1>
-        <h1 class="text-4xl font-font-family text-slate-900">{{ userStore.getUser }}</h1>
+        <q-card class="my-card" flat bordered>
+            <q-card-section>
+                <div class="text-h6 q-mb-xs">Our Changing Planet</div>
+                <div class="row no-wrap items-center">
+                    <q-rating size="18px" v-model="stars" :max="5" color="primary" />
+                    <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
+                </div>
+            </q-card-section>
+
+            <img src="https://cdn.quasar.dev/img/mountains.jpg">
+        </q-card>
     </div>
 
 </template>
