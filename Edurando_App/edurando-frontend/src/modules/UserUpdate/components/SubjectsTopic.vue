29388ea97fa84ca3d9aafd0c84e645858e7505a4
Krish <krish.kalra@stud.th-luebeck.de>
2023-05-23 23:36:47 +0200

    E2: Userprofile Update request

--- Edurando_App/edurando-frontend/src/modules/UserUpdate/components/SubjectsTopic.vue
+++ Edurando_App/edurando-frontend/src/modules/UserUpdate/components/SubjectsTopic.vue
@@ -1,85 +1,66 @@
 <template>
-  <edit-page></edit-page>
-  <div
-      class="min-h-screen bg-gray-100 flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8 font-font-family">
-    <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Add Topic with Subject</h2>
-    <div class="mt-8 space-y-6">
-      <div v-if="showButton" class="flex items-center justify-center font-font-family">
-        <button @click="showButton = false">
-          <font-awesome-icon :icon="['fas', 'circle-plus']" class="mt-20 text-7xl text-[#483d8b]"></font-awesome-icon>
-        </button>
-      </div>
-      <div v-else>
-        <form class="flex flex-col items-center w-full">
-          <div class="w-1/2">
-            <label for="subject" class="text-black font-font-family p-2">Subject</label>
-            <input id="subject" name="subject" type="text" v-model="subject" autocomplete="subject"
-                   class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
-                   placeholder="Enter Subject">
-          </div>
-          <div class="mt-5 w-1/2">
-            <label for="topic" class="text-black font-font-family p-1">Topic</label>
-            <input id="topic" name="topic" type="text" v-model="topic"
-                   class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
-                   placeholder="Enter Topic">
-          </div>
-          <div v-show="saved" class="mt-3 font-font-family text-green-400">
-            <p> Die Daten wurden erfolgreich gespeichert! </p>
-          </div>
-          <div class="flex justify-center mt-7 space-x-4">
-            <div class="mt-6 mr-8 text-center flex justify-center text-white">
-              <button @click="showButton = true">
-                <font-awesome-icon :icon="['fas', 'arrow-left']" class="text-4xl text-gray-900"/>
-              </button>
+    <edit-page></edit-page>
+    <div class="min-h-screen bg-gray-100 flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8 font-font-family">
+        <h3 class="mt-8 text-center text-3xl font-extrabold text-gray-900">Add Topic with Subject</h3>
+        <div class="space-y-6">
+            <div class="flex items-center justify-center font-font-family">
+                <button @click="goSave">
+                    <font-awesome-icon :icon="['fas', 'circle-plus']" class="mt-20 text-7xl text-[#483d8b]"></font-awesome-icon>
+                </button>
             </div>
-            <div class="mt-6 ml-8 text-center flex justify-center text-white">
-              <button @click="addSubjectTopic">
-                <font-awesome-icon :icon="['fas', 'check']" class="text-4xl text-gray-900"/>
-              </button>
+            <div>
+                <h1 class="mt-8 text-center text-3xl font-extrabold text-gray-900">Your Subjects with Topics</h1>
             </div>
-          </div>
-        </form>
-      </div>
+            <div class="flex flex-col items-center text-center">
+                <div v-for="person in people" :key="person.id" class="items-start max-w-xs p-4 mb-4 border-b-2" @click="toggleTopics(person)">
+                    <div class="font-bold text-black">{{ person.subject }}</div>
+                    <ul v-if="person.showTopics" class="mt-2 text-black">
+                        <li v-for="topic in person.topics" :key="topic.id">{{ topic }}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import EditPage from '@/modules/UserUpdate/EditPage.vue';
-import axios from "axios";
 
 export default {
-  components: {
-    FontAwesomeIcon,
-    'editPage': EditPage
-  },
-  data() {
-    return {
-      data: {
-        subject: '',
-        topic: '',
-      },
-      showButton: true,
-      saved: true
+    components: {
+        FontAwesomeIcon,
+        'editPage': EditPage
+    },
+    data() {
+        return {
+            data: {
+                user: {
+                    subject: '',
+                    topic: '',
+                }
+            },
+            showButton: true,
+            saved: true,
+            people: [
+                { id: 1, subject: 'Mathematik', topics: ['Algebra', 'Geometrie'], showTopics: false },
+                { id: 2, subject: 'Informatik', topics: ['Programmierung', 'Datenbanken'], showTopics: false },
+                { id: 3, subject: 'Geschichte', topics: ['Antike Zivilisationen', 'Mittelalter'], showTopics: false }
+            ]
+        };
+    },
+    methods: {
+        async goSave() {
+            try {
+                this.$router.push({ path: '/SubjectsTopicSave' });
+            } catch (error) {
+                console.log(this.error);
+            }
+        },
+        toggleTopics(person) {
+            person.showTopics = !person.showTopics;
+        }
     }
-
-
-  },
-  methods: {
-
-    async addSubjectTopic() {
-      try {
-        const response = await axios.put('/', this.data)
-        response.statusText = response.data
-        this.result = response.data
-        this.$router.push({path: '/'})
-        console.log(response)
-        this.saved = true
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  }
-}
+};
 </script>
+
