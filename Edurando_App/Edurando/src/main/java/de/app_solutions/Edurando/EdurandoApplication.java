ccf5f31246e80a4a460ee3aa387f6444df21aa9e
benne <bennet.gurklies@stud.th-luebeck.de>
2023-05-26 00:31:10 +0100

    #59 search bar functionality

--- Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/EdurandoApplication.java
+++ Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/EdurandoApplication.java
@@ -20,7 +20,7 @@ public class EdurandoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EdurandoApplication.class, args);
 	}
-
+/*
     @Bean
     CommandLineRunner commandLineRunner(UserProfileService userProfileService) {
         return args -> {
@@ -41,6 +41,10 @@ public class EdurandoApplication {
             userProfileService.signUpUser(new UserProfile("Teacher", "William", "Brown", "krish.kalra15@stud.th-luebeck.de", "Test_123", true, 4.0f, "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam"));
         };
     }
+
+ */
+
+
 
 
 }
