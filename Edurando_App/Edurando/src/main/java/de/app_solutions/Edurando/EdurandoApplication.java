f8195d7b8f8b63f3b34966e98dbac5455884b73c
Kalra, Krish <krish.kalra@stud.th-luebeck.de>
2023-05-25 23:42:20 +0000

    Update EdurandoApplication.java

--- Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/EdurandoApplication.java
+++ Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/EdurandoApplication.java
@@ -20,8 +20,8 @@ public class EdurandoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EdurandoApplication.class, args);
 	}
-/*
-    @Bean
+
+    /*@Bean
     CommandLineRunner commandLineRunner(UserProfileService userProfileService) {
         return args -> {
             userProfileService.signUpUser(new UserProfile("Student", "Krish", "Kalra", "krish.kalra@stud.th-luebeck.de", "Test_123", true));
@@ -41,10 +41,4 @@ public class EdurandoApplication {
             userProfileService.signUpUser(new UserProfile("Teacher", "William", "Brown", "krish.kalra15@stud.th-luebeck.de", "Test_123", true, 4.0f, "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam"));
         };
     }*/
-
- */
-
-
-
-
 }
