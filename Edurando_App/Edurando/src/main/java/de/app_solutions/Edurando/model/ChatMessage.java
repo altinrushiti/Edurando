253323ba3ab35f6f2e48463ca0220094667b8279
bennet <bennet.gurklies@stud.th-luebeck.de>
2023-05-29 21:51:19 +0200

    #82 ChatChannel, user1,user2 chat

--- Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/model/ChatMessage.java
+++ Edurando_App/Edurando/src/main/java/de/app_solutions/Edurando/model/ChatMessage.java
@@ -1,36 +1,44 @@
 package de.app_solutions.Edurando.model;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
 public class ChatMessage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @OneToOne
+    @JoinColumn(name = "senderId")
     private UserProfile sender;
 
-    @ManyToOne
+    @OneToOne
+    @JoinColumn(name = "receiverId")
     private UserProfile receiver;
 
+    @NotNull
+    private Date timeSent;
 
-    private String message;
-    private String chatRoomId;
+    @NotNull
+    private String contents;
 
-    public ChatMessage(UserProfile sender, UserProfile receiver, String message) {
+
+    public ChatMessage(UserProfile sender, UserProfile receiver, String contents) {
         this.sender = sender;
         this.receiver = receiver;
-        this.message = message;
+        this.contents = contents;
+        this.timeSent = new Date();
     }
 
 }
