594c89f41c58b072410fc6939b1c1eb8b0119c71
Krish <krish.kalra@stud.th-luebeck.de>
2023-05-31 01:35:42 +0200

    #62: Routes to Login, Logout, Profile, and Sign Up

--- Edurando_App/edurando-frontend/src/store/store.js
+++ Edurando_App/edurando-frontend/src/store/store.js
@@ -1,14 +1,19 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
+import {ref} from "vue";
 
 export const useUserStore = defineStore('user', {
     state: () => ({
         user: null,
+        isLoggedOut: ref(true)
     }),
     persist: true,
     getters: {
         getUser() {
             return this.user;
+        },
+        getIsLoggedOut() {
+            return this.isLoggedOut;
         },
     },
     actions: {
@@ -16,11 +21,14 @@ export const useUserStore = defineStore('user', {
             try {
                 const response = await axios.get(`http://localhost:9001/api/v1/profileByEmail/${email}`);
                 const user = response.data;
+                const isLoggedOut = false
 
                 this.user = user;
+                this.isLoggedOut = isLoggedOut
 
                 // Benutzer im Local Storage speichern
                 localStorage.setItem('user', JSON.stringify(user));
+                localStorage.setItem('isLoggedOut', JSON.stringify(isLoggedOut));
 
                 // Weitere Aktionen ausführen
                 // ...
@@ -32,11 +40,14 @@ export const useUserStore = defineStore('user', {
             try {
                 const response = await axios.get(`http://localhost:9001/api/v1/profile/${id}`);
                 const user = response.data;
+                const isLoggedOut = false
 
                 this.user = user;
+                this.isLoggedOut = isLoggedOut
 
                 // Benutzer im Local Storage speichern
                 localStorage.setItem('user', JSON.stringify(user));
+                localStorage.setItem('isLoggedOut', JSON.stringify(isLoggedOut));
 
                 // Weitere Aktionen ausführen
                 // ...
@@ -44,5 +55,13 @@ export const useUserStore = defineStore('user', {
                 console.error(error);
             }
         },
+        logOut() {
+            const user = null;
+            const isLoggedOut = true
+            this.user = user
+            this.isLoggedOut = isLoggedOut
+            localStorage.setItem('user', JSON.stringify(user));
+            localStorage.setItem('isLoggedOut', JSON.stringify(isLoggedOut));
+        }
     },
 });
