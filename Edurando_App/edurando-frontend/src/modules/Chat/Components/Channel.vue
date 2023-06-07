0ab7470e7762e71f67c4ecc12683dc15204f6d9b
altinrushiti <altin.rushiti@stud.th-luebeck.de>
2023-06-07 12:00:40 +0200

    #81: Chatblasen formartiert

--- Edurando_App/edurando-frontend/src/modules/Chat/Components/Channel.vue
+++ Edurando_App/edurando-frontend/src/modules/Chat/Components/Channel.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="flex items-end justify-end">
-    <div :class="{'relative': isChannel}" class="rounded-lg shadow-lg p-4 max-w-[50%] ">
-      <div class="flex items-start ">
+    <div :class="{'relative': isChannel}" class="rounded-lg shadow-lg p-4 w-[35%]">
+      <div class="flex items-start">
         <div class="flex-shrink-0">
           <img class="h-8 w-8 rounded-full" src="../../../assets/p_placeholder.png" alt="Avatar" />
         </div>
-        <div class="ml-4 ">
-          <div class="mt-1 text-gray-700 ">{{item.contents}}</div>
+        <div class="ml-4">
+          <div class="mt-1 text-gray-700" style="word-break: break-all;">{{item.contents}}</div>
         </div>
       </div>
       <div v-if="isChannel" class="absolute top-0 left-0 h-full w-2 bg-green-500 rounded-l-lg"></div>
     </div>
   </div>
 </template>
+
+
+
+
+
+
+
+
 
 
 <script setup>
