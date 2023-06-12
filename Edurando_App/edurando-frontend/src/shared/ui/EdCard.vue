a06bdc0b460ee36f742bfe382e38c7f4c8b5fe00
Zainoul Abidine Barry <abidine9111@gmail.com>
2023-06-12 21:27:56 +0200

    #102 E3.7 - Karte Schrumpft bei Bio

--- Edurando_App/edurando-frontend/src/shared/ui/EdCard.vue
+++ Edurando_App/edurando-frontend/src/shared/ui/EdCard.vue
@@ -14,10 +14,10 @@ function showModal() {
   emit('show-modal', props.item)
 }
 
+
 </script>
 
 <template>
-
   <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-1 dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <img class="rounded-t-lg" src="../../assets/p_placeholder.png" alt="Profile Photo" />
@@ -26,7 +26,7 @@ function showModal() {
         <a href="#">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{item.firstName + " " + item.lastName}}</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{item.personalBiography}}</p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{item.personalBiography.substring(0,39)}}<span v-if="item.personalBiography.length>42">...</span></p>
         <div class="flex items-center mt-2.5 mb-5">
           <template v-for="star in item.rating">
             <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -40,7 +40,6 @@ function showModal() {
           Read more
           <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </a>
-
       </div>
     </div>
 
