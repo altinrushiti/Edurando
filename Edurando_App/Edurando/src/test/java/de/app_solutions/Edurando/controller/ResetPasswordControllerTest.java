74a9a32079f9662ef125c16aa2cf638eb47ffc40
49176 <ghamdan.al-sofey@stud.th-luebeck.de>
2023-06-14 13:03:58 +0200

    Junit_Test: ResetPasswordServiceTest,ResetPasswordControllerTest .

--- Edurando_App/Edurando/src/test/java/de/app_solutions/Edurando/controller/ResetPasswordControllerTest.java
+++ Edurando_App/Edurando/src/test/java/de/app_solutions/Edurando/controller/ResetPasswordControllerTest.java
@@ -1,133 +1,102 @@
 package de.app_solutions.Edurando.controller;
 
+import de.app_solutions.Edurando.model.ResetPasswordRequest;
 import de.app_solutions.Edurando.service.ResetPasswordService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.util.Pair;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
-
 public class ResetPasswordControllerTest {
-
-    @Mock
-    private ResetPasswordService resetPasswordService;
-
-    @InjectMocks
-    private ResetPasswordController resetPasswordController;
-
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(resetPasswordController).build();
-    }
+    private ResetPasswordService resetPasswordService = mock(ResetPasswordService.class);
+    private ResetPasswordController resetPasswordController = new ResetPasswordController(resetPasswordService);
 
     @Test
-    public void testForgotPassword_Success() throws Exception {
-
+    void testForgotPassword_Successful() {
         String email = "test@example.com";
-        when(resetPasswordService.forgotPassword(email)).thenReturn(Pair.of(true, "Success"));
 
+        when(resetPasswordService.forgotPassword(email)).thenReturn(Pair.of(true, "Email sent"));
 
-        MvcResult result = mockMvc.perform(post("/api/v1/resetPassword")
-                        .param("email", email)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
+        ResponseEntity<String> response = resetPasswordController.forgotPassword(email);
 
-
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Email sent", response.getBody());
         verify(resetPasswordService).forgotPassword(email);
-
     }
 
     @Test
-    public void testForgotPassword_Failure() throws Exception {
-
+    void testForgotPassword_Failure() {
         String email = "test@example.com";
-        when(resetPasswordService.forgotPassword(email)).thenReturn(Pair.of(false, "Failure"));
 
+        when(resetPasswordService.forgotPassword(email)).thenReturn(Pair.of(false, "User not found"));
 
-        MvcResult result = mockMvc.perform(post("/api/v1/resetPassword")
-                        .param("email", email)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andReturn();
+        ResponseEntity<String> response = resetPasswordController.forgotPassword(email);
 
-
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("User not found", response.getBody());
         verify(resetPasswordService).forgotPassword(email);
-
-    }
-    @Test
-    public void testConfirmCode_Successful() {
-        String confirmCode = "1234";
-        String enteredConfirmCode = "1234";
-        Pair<Boolean, String> response = Pair.of(true, "Confirmation was successful.");
-        ResponseEntity<String> expectedResponse = ResponseEntity.ok(response.getSecond());
-
-        when(resetPasswordService.confirmCode(confirmCode, enteredConfirmCode)).thenReturn(response);
-
-        ResponseEntity<String> actualResponse = resetPasswordController.confirmCode(confirmCode, enteredConfirmCode);
-
-        assertEquals(expectedResponse, actualResponse);
     }
 
     @Test
-    public void testConfirmCode_IncorrectCode() {
-        String confirmCode = "1234";
-        String enteredConfirmCode = "5678";
-        Pair<Boolean, String> response = Pair.of(false, "The entered confirmation code is not correct.");
-        ResponseEntity<String> expectedResponse = ResponseEntity.badRequest().body(response.getSecond());
-
-        when(resetPasswordService.confirmCode(confirmCode, enteredConfirmCode)).thenReturn(response);
-
-        ResponseEntity<String> actualResponse = resetPasswordController.confirmCode(confirmCode, enteredConfirmCode);
-
-        assertEquals(expectedResponse, actualResponse);
-    }
-
-    @Test
-    public void testResetPassword_Successful() {
+    void testConfirmCode_Successful() {
         String email = "test@example.com";
-        String newPassword = "newPassword";
-        String newPasswordRepeat = "newPassword";
-        Pair<Boolean, String> response = Pair.of(true, "Password reset was successful.");
-        ResponseEntity<String> expectedResponse = ResponseEntity.ok(response.getSecond());
+        String confirmationCode = "123456";
 
-        when(resetPasswordService.resetPassword(email, newPassword, newPasswordRepeat)).thenReturn(response);
+        when(resetPasswordService.confirmCode(email, confirmationCode)).thenReturn(Pair.of(true, "Code confirmed"));
 
-        ResponseEntity<String> actualResponse = resetPasswordController.resetPassword(email, newPassword, newPasswordRepeat);
+        ResponseEntity<String> response = resetPasswordController.confirmCode(email, confirmationCode);
 
-        assertEquals(expectedResponse, actualResponse);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Code confirmed", response.getBody());
+        verify(resetPasswordService).confirmCode(email, confirmationCode);
     }
 
     @Test
-    public void testResetPassword_PasswordsNotMatch() {
+    void testConfirmCode_Failure() {
         String email = "test@example.com";
-        String newPassword = "newPassword";
-        String newPasswordRepeat = "differentPassword";
-        Pair<Boolean, String> response = Pair.of(false, "Passwords do not match.");
-        ResponseEntity<String> expectedResponse = ResponseEntity.badRequest().body(response.getSecond());
+        String confirmationCode = "123456";
 
-        when(resetPasswordService.resetPassword(email, newPassword, newPasswordRepeat)).thenReturn(response);
+        when(resetPasswordService.confirmCode(email, confirmationCode)).thenReturn(Pair.of(false, "Invalid code"));
 
-        ResponseEntity<String> actualResponse = resetPasswordController.resetPassword(email, newPassword, newPasswordRepeat);
+        ResponseEntity<String> response = resetPasswordController.confirmCode(email, confirmationCode);
 
-        assertEquals(expectedResponse, actualResponse);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Invalid code", response.getBody());
+        verify(resetPasswordService).confirmCode(email, confirmationCode);
+    }
+
+    @Test
+    void testResetPassword_Successful() {
+        ResetPasswordRequest request = new ResetPasswordRequest();
+        request.setEmail("test@example.com");
+        request.setNewPassword("newPassword");
+
+        when(resetPasswordService.resetPassword(request)).thenReturn(Pair.of(true, "Password reset"));
+
+        ResponseEntity<String> response = resetPasswordController.resetPassword(request);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Password reset", response.getBody());
+        verify(resetPasswordService).resetPassword(request);
+    }
+
+    @Test
+    void testResetPassword_Failure() {
+        ResetPasswordRequest request = new ResetPasswordRequest();
+        request.setEmail("test@example.com");
+        request.setNewPassword("newPassword");
+
+        when(resetPasswordService.resetPassword(request)).thenReturn(Pair.of(false, "User not found"));
+
+        ResponseEntity<String> response = resetPasswordController.resetPassword(request);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("User not found", response.getBody());
+        verify(resetPasswordService).resetPassword(request);
     }
 }
